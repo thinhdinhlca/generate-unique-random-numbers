1512d8f2d764665062601e0f0d4e1f2b38aa5cd9
Thịnh Đinh <94897059+thinhdinhlca@users.noreply.github.com>
2022-02-19 16:44:37 +0700

    Update function.js

--- function.js
+++ function.js
@@ -1,7 +1,8 @@
-window.function = function (quantity, max) {
+window.function = function (quantity, max, timestamp) {
   
   quantity = quantity.value ?? "1";
   max = max.value ?? "1000";
+  timestamp = timestamp.value ?? "";
   
   const arr = []
   while(arr.length < quantity){
